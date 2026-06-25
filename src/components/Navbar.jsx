ebac847aec374dbdf5357a2909607033f7655446
Lucho Olivera <luis.olivera1902@gmail.com>
2026-06-24 21:37:07 -0400

    Agrega manifest e iconos PWA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-primary rounded mb-4">
+    <nav
+      className="navbar navbar-dark rounded mb-4"
+      style={{
+        background:
+          "linear-gradient(90deg,#0c055c 0%,#2d6eb3 35%,#f28c28 100%)",
+      }}
+    >
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">
           🎵 Repertorio CEHCJ
